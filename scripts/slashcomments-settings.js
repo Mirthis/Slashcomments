edccbcac67cf9443bf8191d02a59da39a7d74805
Mirthis <mirthis@gmail.com>
2011-04-25 11:56:26 +0200

    Moved some files around

---
+++ scripts/slashcomments-settings.js
@@ -0,0 +1,14 @@
+
+Drupal.behaviors.slashcomments = function(context) {
+    slashcomments_toggle_takepart();
+    slashcomments_toggle_karma();
+};
+
+function slashcomments_toggle_takepart() {
+  $('#edit-slashcomments-delete-takepart').attr('disabled',!$('#edit-slashcomments-limit-takepart').is(':checked'));
+}
+
+function slashcomments_toggle_karma() {
+  $('#edit-slashcomments-limit-karma').attr('disabled',!$('#edit-slashcomments-enable-karma').is(':checked'));
+}
+
